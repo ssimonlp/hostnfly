3228f8faa003b6518d79d15fc031f5f0b06965f9
Simon Le pavec <simon@MacBook-Pro-de-Simon.local>
2021-09-11 16:06:50 +0200

    add basic reservation model

---
+++ hostnfly_api/db/migrate/20210911140510_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reservations do |t|
+      t.references :listing, null: false, foreign_key: true, index:true
+      t.date :start_date, null: false
+      t.date :end_date, null: false
+      t.boolean :is_active, null: false
+
+      t.timestamps
+    end
+  end
+end
